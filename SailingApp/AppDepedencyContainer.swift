2bd699785ec66fe0e72b0674980e3d9f4753c03d
Krzysztof <Krzysztof.Lema@hemmersbach.com>
2020-08-29 10:42:24 +0200

    no message

---
+++ SailingApp/AppDepedencyContainer.swift
@@ -0,0 +1,12 @@
+//
+//  AppDepedencyContainer.swift
+//  SailingApp
+//
+//  Created by Krzysztof Lema on 29/08/2020.
+//  Copyright © 2020 Krzysztof Lema. All rights reserved.
+//
+
+import Foundation
+public class AppDepedencyContainer {
+    
+}
