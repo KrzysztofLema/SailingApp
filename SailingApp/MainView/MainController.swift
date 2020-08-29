6387167e7f460419ff66499174ece60c97cd336e
Krzysztof <Krzysztof.Lema@hemmersbach.com>
2020-08-29 11:54:41 +0200

    separated view from view controller

---
+++ SailingApp/MainView/MainController.swift
@@ -0,0 +1,25 @@
+//
+//  MainController.swift
+//  SailingApp
+//
+//  Created by Krzysztof Lema on 29/08/2020.
+//  Copyright © 2020 Krzysztof Lema. All rights reserved.
+//
+
+import UIKit
+class MainController: UIViewController {
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+        view = MainView()
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ SailingApp/MainView/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SailingApp
+//
+//  Created by Krzysztof Lema on 14/08/2020.
+//  Copyright © 2020 Krzysztof Lema. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView  {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .green
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
