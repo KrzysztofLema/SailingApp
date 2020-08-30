1f8fe31c56c701db68be5753dc81f1612682d954
Krzysztof <Krzysztof.Lema@hemmersbach.com>
2020-08-30 11:04:14 +0200

    move views and view controllers to proper place

---
+++ SailingApp_iOS/SailingApp_iOS.h
@@ -0,0 +1,19 @@
+//
+//  SailingApp_iOS.h
+//  SailingApp_iOS
+//
+//  Created by Krzysztof Lema on 29/08/2020.
+//  Copyright © 2020 Krzysztof Lema. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SailingApp_iOS.
+FOUNDATION_EXPORT double SailingApp_iOSVersionNumber;
+
+//! Project version string for SailingApp_iOS.
+FOUNDATION_EXPORT const unsigned char SailingApp_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SailingApp_iOS/PublicHeader.h>
+
+
